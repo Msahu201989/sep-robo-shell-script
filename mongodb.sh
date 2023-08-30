915c0a9038515cc244337fbd2cb9d534e3094366
Mukesh.Sahu <msahu201989@gmail.com>
2023-08-30 21:14:34 +0530

    Variables in log file

---
+++ mongodb.sh
@@ -0,0 +1,22 @@
+LOG_FILE=/tmp/mogodb
+echo "Setting up MogoDB Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
+echo status = $?
+
+echo "Installing MongoDB Server"
+yum install -y mongodb-org &>>LOG_FILE
+echo status = $?
+
+echo "Starting Mongodb Service"
+ systemctl enable mongod &>>LOG_FILE
+ systemctl restart mongod &>>LOG_FILE
+ echo status = $?
+
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
+
