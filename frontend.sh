87137650cae886afaeba12d84ea416dca4c0138c
Mukesh.Sahu <msahu201989@gmail.com>
2023-08-30 11:15:44 +0530

    tes

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,20 @@
- yum install nginx -y
+echo installing Nginx
+yum install nginx -y
 
-
+echo downloading nginx web content
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
  cd /usr/share/nginx/html
+
+ echo removing old web content
  rm -rf *
+
+ echo Extracting web content
  unzip /tmp/frontend.zip
+
  mv frontend-main/static/* .
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+
+ echo starting Nginx Service
 systemctl enable nginx
 systemctl restart nginx
