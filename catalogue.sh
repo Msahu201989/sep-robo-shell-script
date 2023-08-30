b63b3f586fb8ea12c098822e276a87bfb6450e9e
Mukesh.Sahu <msahu201989@gmail.com>
2023-08-30 22:28:36 +0530

    Variables in log file

--- catalogue.sh
+++ catalogue.sh
@@ -1,23 +1,23 @@
 LOG_FILE=/tmp/catalogue
 echo "Setup NodeJS REPO"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>LOG_FILE
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>$LOG_FILE
 echo status = $?
 
 echo "INSTALL NODEJS"
-yum install nodejs -y &>>LOG_FILE
+yum install nodejs -y &>>$LOG_FILE
 
 echo "Creating Roboshop USER"
 useradd roboshop
 echo status = $?
 
 echo "Download Catalogue Application Code"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
 echo status = $?
 
 cd /home/roboshop
 
 echo "Extracting Catalogue Application code"
-unzip /tmp/catalogue.zip &>>LOG_FILE
+unzip /tmp/catalogue.zip &>>$LOG_FILE
 echo status = $?
 
 
@@ -26,15 +26,15 @@ cd /home/roboshop/catalogue
 echo status = $?
 
 echo "Installing Nodejs Dependencies"
-npm install &>>LOG_FILE
+npm install &>>$LOG_FILE
 echo status = $?
 
 #MONGO_DNSNAME
 
 echo "Setup Catalogue Service"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>LOG_FILE
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
 echo status = $?
 
-systemctl daemon-reload &>>LOG_FILE
- systemctl start catalogue &>>LOG_FILE
- systemctl enable catalogue &>>LOG_FILE
+systemctl daemon-reload &>>$LOG_FILE
+ systemctl start catalogue &>>$LOG_FILE
+ systemctl enable catalogue &>>$LOG_FILE
