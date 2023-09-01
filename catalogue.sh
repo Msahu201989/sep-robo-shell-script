278cb23d9bf9448f5a318a4cafeea07fa25a94b9
Mukesh.Sahu <msahu201989@gmail.com>
2023-09-01 10:35:03 +0530

    Variables in log file

--- catalogue.sh
+++ catalogue.sh
@@ -3,6 +3,8 @@ echo "Setup NodeJS REPO"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>$LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = SUCESS
+else
+  echo status = FAILURE
   fi
 
 
@@ -13,12 +15,16 @@ echo "Creating Roboshop USER"
 useradd roboshop
 if [ $? -eq 0 ]; then
   echo status = SUCESS
+else
+  echo status = FAILURE
   fi
 
 echo "Download Catalogue Application Code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = SUCESS
+else
+  echo status = FAILURE
   fi
 
 cd /home/roboshop
@@ -27,16 +33,23 @@ echo "Extracting Catalogue Application code"
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = SUCESS
+else
+  echo status = FAILURE
   fi
-
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo status = SUCESS
+else
+  echo status = FAILURE
+  fi
 
 echo "Installing Nodejs Dependencies"
 npm install &>>$LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = SUCESS
+else
+  echo status = FAILURE
   fi
 
 #MONGO_DNSNAME
@@ -45,6 +58,8 @@ echo "Setup Catalogue Service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = SUCESS
+else
+  echo status = FAILURE
   fi
 
 systemctl daemon-reload &>>$LOG_FILE
