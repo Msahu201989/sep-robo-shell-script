8adedd71e2c06366e5527e8aa476c25b915af6fa
Mukesh.Sahu <msahu201989@gmail.com>
2023-09-04 10:26:16 +0530

    Variables in log file

--- redis.sh
+++ redis.sh
@@ -9,11 +9,10 @@ StatusCheck $?
 
 echo "Installing Redis"
 yum install redis-6.2.11 -y &>>$LOG_FILE
-
+StatusCheck $?
 
 
 echo "Update Redis IP in Config from 127.0.0.1 to 0.0.0.0"
-
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
 StatusCheck $?
 
