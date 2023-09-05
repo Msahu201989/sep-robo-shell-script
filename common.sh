22635d2d88ab8e9017a6fb38959e290660cd1b65
Mukesh.Sahu <msahu201989@gmail.com>
2023-09-05 10:23:35 +0530

    Variables in log file

--- common.sh
+++ common.sh
@@ -12,3 +12,59 @@ else
   exit 1
   fi
 }
+
+NODEJS() {
+
+  echo "Downlod NodeJs Component"
+   curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>$LOG_FILE
+   StatusCheck $?
+
+   echo " Installing Nodejs"
+   yum install nodejs -y &>>$LOG_FILE
+   StatusCheck $?
+
+  id roboshop &>>$LOG_FILE
+  if [ $? -ne 0 ]; then
+   echo "Adding Roboshop User"
+  useradd roboshop &>>$LOG_FILE
+  StatusCheck $?
+   fi
+
+
+   echo "Download ${COMPONENT} Application Code"
+   curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG_FILE
+   StatusCheck $?
+
+   cd /home/roboshop
+   echo "clean old App content"
+   rm -rf ${COMPONENT} &>>$LOG_FILE
+   StatusCheck $?
+
+   echo "Extracting ${COMPONENT} Application code"
+   unzip -o /tmp/${COMPONENT}.zip &>>$LOG_FILE
+   StatusCheck $?
+
+   echo "Moving App data"
+   mv user-main ${COMPONENT} &>>$LOG_FILE
+   cd /home/roboshop/${COMPONENT}
+   StatusCheck $?
+
+   echo "Installing Nodejs Dependencies"
+   npm install &>>$LOG_FILE
+   StatusCheck $?
+
+   echo "Updating SystemD service File"
+   sed -i -e 's/Redis_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE}
+   StatusCheck $?
+
+    echo "Setup ${COMPONENT} Service"
+    mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
+    StatusCheck $?
+
+    echo "Start ${COMPONENT} service"
+     systemctl daemon-reload &>>${LOG_FILE}
+     systemctl start ${COMPONENT} &>>${LOG_FILE}
+     systemctl enable ${COMPONENT} &>>${LOG_FILE}
+     StatusCheck $?
+
+}
