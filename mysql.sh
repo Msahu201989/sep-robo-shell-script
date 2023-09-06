bac563a269efa6ceb2df667238d8dae064976062
Mukesh.Sahu <msahu201989@gmail.com>
2023-09-06 10:06:21 +0530

    Variables in log file

--- mysql.sh
+++ mysql.sh
@@ -3,34 +3,50 @@ source common.sh
 
 echo "Setting up MYSQL REPO"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
-StatusCheck $?
+ StatusCheck $?
 
 echo "Install MYSQL"
  yum install mysql-community-server -y &>>$LOG_FILE
- StatusCheck $?
+  StatusCheck $?
 
  echo "Start MYSQL Service"
  systemctl enable mysqld &>>$LOG_FILE
  systemctl restart mysqld &>>$LOG_FILE
- StatusCheck $?
+  StatusCheck $?
 
 echo "seting up password"
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
- StatusCheck $?
+  StatusCheck $?
 
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD
 ('${ROBOSHOP_MYSQL_PASSWORD}');
 FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
-# grep temp /var/log/mysqld.log
-#
-##Next, We need to change the default root password in order to start using the database service. Use password RoboShop@1 or any other as per your choice. Rest of the options you can choose No
-#
-# mysql_secure_installation
-# mysql -uroot -pRoboShop@1
-##> uninstall plugin validate_password;
-# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-# cd /tmp
-# unzip mysql.zip
-# cd mysql-main
-# mysql -u root -pRoboShop@1 <shipping.sql
+echo "show databases;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}  &>>LOG_FILE
+if [ $? -ne 0 ]; then
+echo "Change the default password "
+mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>LOG_FILE
+ StatusCheck $?
+fi
+
+echo 'show plugins'| mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} 2>>$LOG_FILE | grep validate_password &>>LOG_FILE
+if [ $? -eq 0 ]; then
+echo "uninstall password validation plugin"
+echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>LOG_FILE
+ StatusCheck $?
+fi
+
+echo "download schema"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>LOG_FILE
+ StatusCheck $?
+
+echo "extract file"
+cd /tmp
+unzip -o mysql.zip &>>LOG_FILE
+ StatusCheck $?
+
+echo "load schema"
+cd mysql-main
+mysql -u root -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>LOG_FILE
+ StatusCheck $?
+
