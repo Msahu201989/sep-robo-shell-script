5b391e7750c1e6de32eab257ddffafc6ef89094f
Mukesh.Sahu <msahu201989@gmail.com>
2023-09-07 11:03:53 +0530

    Variables in log file

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,13 +2,17 @@ COMPONENT=rabbitmq
 source common.sh
 LOG_FILE=/tmp/${COMPONENT}
 
-echo "setup rabbitmq repo"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$LOG_FILE
- StatusCheck $?
+
 
 echo "Install Erland & Rabbitmq"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v25.1/erlang-25.1-1.el8.x86_64.rpm rabbitmq-server -y &>>$LOG_FILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$LOG_FILE
+yum install erlang -y &>>$LOG_FILE
  StatusCheck $?
+
+ echo "setup rabbitmq repo"
+ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$LOG_FILE
+ yum install rabbitmq-server -y &>>$LOG_FILE
+  StatusCheck $?
 
 echo "start rabbitmq server"
 systemctl enable rabbitmq-server &>>LOG_FILE
